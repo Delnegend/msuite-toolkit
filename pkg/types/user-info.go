8da5e2bac652ea4128242e1473335b81f22b43b7
Delnegend <kiennguyen19323@gmail.com>
2026-03-05 10:00:38 +0700

    feat(`types/user-info`): add more info fields

--- pkg/types/user-info.go
+++ pkg/types/user-info.go
@@ -1,6 +1,19 @@
 package types
 
 type UserInfo struct {
-	UserID    string `json:"user_id"`
-	UserEmail string `json:"email"`
+	UserID         string            `json:"user_id"`
+	Name           string            `json:"name"`
+	DisplayName    string            `json:"display_name"`
+	UserEmail      string            `json:"email"`
+	PhoneNumber    string            `json:"phone_number"`
+	Roles          []string          `json:"roles"`
+	OwnerInfo      any               `json:"owner_info"`
+	Meta           any               `json:"meta"`
+	Attributes     map[string]string `json:"attributes"`
+	LoginSessionID string            `json:"login_session_id"`
+	IsLocked       bool              `json:"is_locked"`
+	Description    string            `json:"description"`
+	Type           string            `json:"type"`
+	Language       string            `json:"language"`
+	CreatedTime    int64             `json:"created_time"`
 }
