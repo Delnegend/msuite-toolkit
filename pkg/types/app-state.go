ddf84321f7fbc15d909ac2b1d29dfeb4a8e5cb34
Delnegend <kiennguyen19323@gmail.com>
2026-05-31 18:55:53 +0700

    refactor(`types/app-state`): reorder fields in `AppState` struct for improved readability

--- pkg/types/app-state.go
+++ pkg/types/app-state.go
@@ -1,10 +1,17 @@
 package types
 
 type AppState struct {
-	AdminPortalAddress string `toml:"admin_portal_address" json:"admin_portal_address"`
-	BearerToken        string `toml:"bearer_token" json:"bearer_token"`
-	AdminUserID        string `toml:"admin_user_id" json:"admin_user_id"`
-	WorkerCount        int    `toml:"worker_count" json:"worker_count"`
+	AdminPortalAddress               string `toml:"admin_portal_address" json:"admin_portal_address"`
+	BearerToken                      string `toml:"bearer_token" json:"bearer_token"`
+	AdminUserID                      string `toml:"admin_user_id" json:"admin_user_id"`
+	WorkerCount                      int    `toml:"worker_count" json:"worker_count"`
+	DryRun                           bool   `toml:"dry_run" json:"dry_run"`
+	IncludeUsersWithUnknownLastLogin bool   `toml:"include_users_with_unknown_last_login" json:"include_users_with_unknown_last_login"`
+
+	FilterBy struct {
+		DestinationHost string `toml:"destination_host" json:"destination_host"`
+		DestinationPort string `toml:"destination_port" json:"destination_port"`
+	} `toml:"filter_by" json:"filter_by"`
 
 	// last_login_threshold_in_month: discard users not logged in within this many months (0 = disabled)
 	LastLoginThresholdInMonths int `toml:"last_login_threshold_in_month" json:"last_login_threshold_in_month"`
